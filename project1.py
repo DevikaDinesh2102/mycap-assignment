fccf8c5fba5e20599d0600b3a09bd4cb3ccc71de
DevikaDinesh2102 <97044355+DevikaDinesh2102@users.noreply.github.com>
2022-01-19 16:06:39 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 19 13:27:32 2022
+
+@author: HP
+"""
+#1 area of the circle
+radius=int(input("radius of the circle:"))
+pi=3.14
+Area=pi*radius**2
+print("area of the circle:",Area)
+
+
+
+#2 file extension 
+filename=input("filename:")
+ext=filename.split(".")[-1]
+print("ext:",ext)
